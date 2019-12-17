b6afc1c6922d1e329ba0794f3d8916ea6556e09d
Rex-Chiang <39951287+Rex-Chiang@users.noreply.github.com>
2019-12-17 21:38:55 +0800

    Deploy test

--- LineBot/views.py
+++ LineBot/views.py
@@ -3,13 +3,13 @@ from django.conf import settings
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
 
-from linebot import LineBotApi, WebhookHandler
+from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextSendMessage
 # 這邊是Linebot的授權TOKEN(等等註冊LineDeveloper帳號會取得)，
 # 我們為DEMO方便暫時存在settings裡面存取，實際上使用的時候記得設成環境變數，不要公開在程式碼裡喔！
-line_bot_api = LineBotApi(settings.CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(settings.CHANNEL_SECRET)
+line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
+parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
 @csrf_exempt
 def callback(request):
@@ -19,7 +19,7 @@ def callback(request):
         body = request.body.decode('utf-8')
 
         try:
-            events = handler.handler(body, signature)
+            events = parser.parse(body, signature)
         except InvalidSignatureError:
             return HttpResponseForbidden()
         except LineBotApiError:
